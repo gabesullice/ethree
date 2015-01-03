1f86e45a02a03ec07b29825457b8cf7ef82a097a
gabesullice <gabriel@sullice.com>
2015-01-02 19:51:51 -0700

    Add --root flag to new module command.

--- new.go
+++ new.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"github.com/jwaldrip/odin/cli"
+	"os"
+	"path"
+	"strings"
 )
 
 func commandNew() *cli.SubCommand {
@@ -17,7 +19,12 @@ func commandNew() *cli.SubCommand {
 	module.DefineStringFlag(
 		"directory",
 		"sites/all/modules/custom",
-		"A directory path relative to the current Drupal root. Defaults to sites/all/modules/custom.",
+		"A directory path relative to the current Drupal root.",
+	)
+	module.DefineStringFlag(
+		"root",
+		"/path/to/drupal",
+		"A full path to a Drupal install directory.",
 	)
 
 	cmd.DefineSubCommand("site", "Creates a new site skel.", newSite, "name")
@@ -28,10 +35,32 @@ func commandNew() *cli.SubCommand {
 func newModule(c cli.Command) {
 	var name = c.Param("name")
 
-	root, err := drupalRoot()
+	rootFlag := fmt.Sprintf("%s", c.Flag("root"))
+
+	if rootFlag == "/path/to/drupal" {
+
+		var err error
+		rootFlag, err = os.Getwd()
+
+		if err != nil {
+			fmt.Println("Could not get the current directory.")
+			return
+		}
+	} else {
+		rootFlag = path.Clean(rootFlag)
+
+		if strings.Contains(rootFlag, "~") {
+			fmt.Println("Root must be a fully specified path. E.g. it should not contain a \"~\".")
+			return
+		}
+	}
+
+
+	root, err := drupalRoot(rootFlag)
 
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println("You may want to try again with the --root flag.")
 		return
 	}
 
--- utils.go
+++ utils.go
@@ -3,28 +3,15 @@ package main
 import (
 	"errors"
 	"os"
-	p "path"
+	"path"
 )
 
-func drupalRoot(path ...string) (root string, err error) {
-	var curr string
-
-	if len(path) == 0 {
-		var getwdErr error
-
-		curr, getwdErr = os.Getwd()
-
-		if getwdErr != nil {
-			return "", errors.New("Could not get the current directory.\n")
-		}
-	} else {
-		curr = path[0]
-	}
+func drupalRoot(curr string) (root string, err error) {
 
 	var _, fileErr = os.Stat(curr + "/includes/bootstrap.inc")
 
 	if fileErr != nil {
-		dir := p.Dir(curr)
+		dir := path.Dir(curr)
 
 		if dir == "/" {
 			err = errors.New("Not inside of a Drupal installation.\n")
